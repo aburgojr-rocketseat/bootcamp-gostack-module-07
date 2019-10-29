15ed0ad2f7808251e2e2bc19dbc772c9eca0e026
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2019-10-29 00:54:35 -0300

    styling components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyles from './styles/global';
 import Routes from './routes';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes />
+      <GlobalStyles />
     </BrowserRouter>
   );
 }
